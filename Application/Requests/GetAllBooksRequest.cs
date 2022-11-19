9d3f40f29573c5fdd37c7fa7b6d2a5186caa45bb
FSorek <filip.sorek96@gmail.com>
2022-11-19 21:06:21 +0100

    Added MediatR

---
+++ Application/Requests/GetAllBooksRequest.cs
@@ -0,0 +1,24 @@
+﻿using Books.Entities;
+using Books.Repositories;
+using MediatR;
+
+namespace Application.Requests;
+
+public class GetAllBooksRequestHandler : IRequestHandler<GetAllBooksRequest, IEnumerable<Book>>
+{
+    private readonly IBookRepository _bookRepository;
+
+    public GetAllBooksRequestHandler(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+    public Task<IEnumerable<Book>> Handle(GetAllBooksRequest request, CancellationToken cancellationToken)
+    {
+        var books = _bookRepository.GetAllAsync();
+        return books;
+    }
+}
+public class GetAllBooksRequest : IRequest<IEnumerable<Book>>
+{
+    
+}
--- BlazorWebApp/Program.cs
+++ BlazorWebApp/Program.cs
@@ -1,13 +1,17 @@
+using Application.Requests;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebApp;
+using Books.Repositories;
 using Infrastructure.Repositories;
+using MediatR;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-builder.Services.AddScoped<BookRepository>();
+builder.Services.AddMediatR(typeof(GetAllBooksRequest));
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 await builder.Build().RunAsync();
